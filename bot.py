5861b428f163eefdc7f457fac18f56959ba765b7
Foeegarten <77509969+Foeegarten@users.noreply.github.com>
2021-05-18 20:16:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,9 @@ onlstream_ = False
 async def send_message(channel_id: int,msg):
     channel = client.get_channel(channel_id)
     await channel.send(msg)
+phrases = ['@everyone оо нихуя там кебабобка подрубил все бегом смотреть https://wasd.tv/kebabobka ','@everyone Ready steady хуй на блюде https://wasd.tv/kebabobka','@everyone идем массово чалавать песок https://wasd.tv/kebabobka','@everyone Эйбан рот ето подруб ода ода ода https://wasd.tv/kebabobka','@everyone Пацаны пацаны эй https://wasd.tv/kebabobka',
+'@everyone Фиксируем прибыль https://wasd.tv/kebabobka ','@everyone Че тааааам https://wasd.tv/kebabobka','@everyone Че такой серьезный? - улыбнулся - воооо)))) https://wasd.tv/kebabobka','@everyone Оаоаоаоао ммммм подруб мммммм https://wasd.tv/kebabobka','@everyone Я...ммм...пук...подр....подруб....мммм....пук ... https://wasd.tv/kebabobka','@everyone Скука падлы покой дуры мы фанаты подруба кебабуры хдхдхдхдддд https://wasd.tv/kebabobka',
+'@everyone ЛЭЙ ЛЭЙ НЕ ЖАЛЭЙ https://wasd.tv/kebabobka']
 @client.command()
 async def help(ctx):
     embed=discord.Embed(title="Комманды", color=0xff0000)
@@ -47,7 +50,7 @@ async def ready():
 async def doin(onlstream):
     while True:
         if onlstream==True:
-            await send_message(826967699082969088,random.choice(alpha.phrases))
+            await send_message(826967699082969088,random.choice(phrases)
             print('[log] stream is online')
             timestart = t.time()
             await asyncio.sleep(15000)
