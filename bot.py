84d528a2308b0c6c0f83cafa729a6d37260691f0
Foeegarten <77509969+Foeegarten@users.noreply.github.com>
2021-05-19 15:16:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from typing import Optional
 from discord.utils import get
 players = {}
 client = commands.Bot(command_prefix="!",intents = discord.Intents.all(),help_command=None)
-url = 'https://wasd.tv/kebabobka'
+url = 'https://wasd.tv/pugchomp'
 onlstream_ = False
 async def send_message(channel_id: int,msg):
     channel = client.get_channel(channel_id)
@@ -51,9 +51,8 @@ async def doin(onlstream):
     while True:
         if onlstream==True:
             print('[log] stream is online')
-            await send_message(826967699082969088,random.choice(phrases))
-            timestart = t.time()
-            await asyncio.sleep(15000)
+            await send_message(841409828470652950,random.choice(phrases))
+            await asyncio.sleep(10)
         else:
             print('[log] stream is offline')
             await asyncio.sleep(120)
