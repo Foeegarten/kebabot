52449aa07b88b6c8d21fbf5282f74ae60334bd25
Foeegarten <77509969+Foeegarten@users.noreply.github.com>
2021-05-30 09:27:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -190,8 +190,8 @@ async def top(ctx):
         for member in guild.members:
             data = collection.find_one({"_id":member.id})
             spisok.append(data['points'])
-    spisok = set(spisok)
     spisok=sorted(spisok,reverse=True)
+    spisok = set(spisok)
     for x in range(10):
         pointy = collection.find_one({"points":spisok[x]})
         spiso4ek.append(f"У {(client.get_user(pointy['_id'])).display_name} {pointy['points']} очков")
