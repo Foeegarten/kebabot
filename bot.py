0b7a9783e27845e468c06da63ca610c123e2db17
Foeegarten <77509969+Foeegarten@users.noreply.github.com>
2021-06-04 00:52:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ async def anek(ctx):
 
 @client.listen('on_member_join')
 async def on_member_join(member):
-    role = discord.utils.get(guild.roles,name='Клерк')
+    role = discord.utils.get(guild.roles,id=827141363769278475)
     await member.add_roles(role)
     post={
     "_id": member.id,
