6ce663f21b9d41f0899b44cc57ec5c678e0cbd40
Foeegarten <77509969+Foeegarten@users.noreply.github.com>
2021-06-03 12:43:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -183,7 +183,7 @@ async def points(ctx,*,member:discord.Member=None):
     data = collection.find_one({"_id":member.id})
     await ctx.send(f"У {member.display_name} {data['points']} очков")
 @client.command()
-@cooldown(1,600,BucketType.user)
+@cooldown(1,60,BucketType.user)
 async def heal(ctx,*,member:discord.Member=None):
     ydata = collection.find_one({"_id":member.id})
     choice_=random.randint(1,100)
